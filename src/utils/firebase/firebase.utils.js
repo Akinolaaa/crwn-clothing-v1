52c81b33291c0b51ae3260e6524418861bdd1794
Akinola Akinleye <akin.akinleye619@gmail.com>
2022-04-19 23:58:42 +0100

    added and called categories from firestore, created preview and category page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,11 @@ import {
   GoogleAuthProvider,
   signOut,
   onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  doc, getDoc, 
+  setDoc, collection, 
+  writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGUu7iEuUIQLtITcuFhrcmRwpOy6HZquk",
@@ -33,6 +37,32 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  // Transaction is a word that represents a successful unit of work to a db
+  const batch = writeBatch(db);
+  objectsToAdd.forEach(object => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object)
+  });
+  await batch.commit();
+  console.log('done adding collection x docs');
+}
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+  return categoryMap;
+}
+
 
 export const createUserDocumentFromAuth = async (
   userAuth, additionalInformation={}) => {
